efcd997796afbb87e42dbe5018bee7292edf3043
Salavat Abdoulline <sha1-8e38bffa4b6141c3b05a505ddf6b2603102043d9@cern.ch>
2009-01-09 10:38:12 +0000

    fixing omission in noise(DB param) treatment and moving Geometry to _cfg.py

--- test/HBDigiTester_cfg.py
+++ test/HBDigiTester_cfg.py
@@ -2,6 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("HCALDIGISVALIDATION")
 process.load("Validation.HcalDigis.HcalDigiTester_cfi")
+process.load("Configuration.StandardSequences.GeometryHCAL_cff")
 
 process.PoolSource.fileNames = ['file:/afs/cern.ch/cms/data/CMSSW/Validation/HcalDigis/data/2_1_0/simhits_pi50_etaphi44.root']
 
