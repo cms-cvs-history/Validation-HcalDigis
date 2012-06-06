44378a061366f43f3944b107ec129c97d77e6676
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2012-06-06 21:08:46 +0000

    back out change made obsolete by EDAlias feature

--- python/HcalDigiParam_cfi.py
+++ python/HcalDigiParam_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 hcalDigiAnalyzer = cms.EDAnalyzer("HcalDigiTester",
-    digiLabel = cms.InputTag("mix", "simHcalUnsuppressedDigis"),
+    digiLabel = cms.InputTag("simHcalUnsuppressedDigis"),
     zside = cms.untracked.string('*'),
     outputFile = cms.untracked.string(''),
     hcalselector = cms.untracked.string('HB')
